a6457f0d8e728c23b5fc8846702311e8858560a1
OleksiiAnokhin2023 <anokhin.oleksii@lll.kpi.ua>
2025-12-20 00:23:32 +0200

    Update README and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import js from '@eslint/js';
-import parser from '@typescript-eslint/parser';
-import plugin from '@typescript-eslint/eslint-plugin';
+import js from "@eslint/js";
+import parser from "@typescript-eslint/parser";
+import plugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   // Базові правила JavaScript
@@ -8,55 +8,55 @@ export default [
 
   // TypeScript файли
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     languageOptions: {
       parser,
       parserOptions: {
         ecmaVersion: 2020,
-        sourceType: 'module',
+        sourceType: "module",
       },
       globals: {
-        process: 'readonly',
-        console: 'readonly',
+        process: "readonly",
+        console: "readonly",
       },
     },
     plugins: {
-      '@typescript-eslint': plugin,
+      "@typescript-eslint": plugin,
     },
     rules: {
       // ❗ спеціально вимикаємо, щоб не конфліктував з prettier
-      quotes: 'off',
-      semi: 'off',
-      'no-undef': 'off',
-      'no-unused-vars': 'off',
+      quotes: "off",
+      semi: "off",
+      "no-undef": "off",
+      "no-unused-vars": "off",
     },
   },
 
   // Тести Jest
   {
-    files: ['**/*.spec.ts'],
+    files: ["**/*.spec.ts"],
     languageOptions: {
       globals: {
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
       },
     },
   },
 
   // Node.js конфіги
   {
-    files: ['*.js'],
+    files: ["*.js"],
     languageOptions: {
       globals: {
-        module: 'readonly',
-        require: 'readonly',
+        module: "readonly",
+        require: "readonly",
       },
     },
     rules: {
-      'no-undef': 'off',
+      "no-undef": "off",
     },
   },
 ];
