37ba53dbd8af9b6f6a8fae183911638f2ed57548
OleksiiAnokhin2023 <anokhin.oleksii@lll.kpi.ua>
2025-12-20 02:13:15 +0200

    yml upd

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,28 @@ app.use("/subjects", subjectsController);
 app.use("/tasks", tasksController);
 app.use("/users", usersController);
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log("База даних успішно підключена");
+
+//     app.listen(PORT, () => {
+//       console.log(`Сервер запущено на порту ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Помилка під час ініціалізації бази даних:", error);
+//   });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Сервер працює на порту ${PORT}`);
+});
 
 AppDataSource.initialize()
   .then(() => {
     console.log("База даних успішно підключена");
-
-    app.listen(PORT, () => {
-      console.log(`Сервер запущено на порту ${PORT}`);
-    });
   })
   .catch((error) => {
-    console.error("Помилка під час ініціалізації бази даних:", error);
+    console.error("Помилка БД (локальна база недоступна):", error);
   });
